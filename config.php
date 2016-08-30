c44970a9610d59b08fcfde8242cd887f4360e7aa
PriyankaBolisetty <PriyankaBolisetty@users.noreply.github.com>
2016-08-29 19:02:25 -0700

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $username = "root";
-    $password = "M@sters15intern";
-    $database = "yelp-dataset";
-    $localhost = "127.0.0.1";
-    
-    $initialQuery = "SELECT * FROM businessSeasons where city='Phoenix' LIMIT 5";
-    $seasonQuery = "SELECT DISTINCT season FROM businessSeasons";
-?>
